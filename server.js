429e549fb4dedf07aaf55cc1623872c6f9445414
B.Accessories <baccessories@example.com>
2026-05-01 13:34:42 +0300

    Change default port from 3000 to 3001

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const dataDir = path.join(__dirname, 'data');
 const dataFile = path.join(dataDir, 'site-data.json');
 
